80e8acf13d382f53e671202fce0c4ff3b0d36c27
Alexander Makedonski <alemak2002@gmail.com>
2023-03-23 20:03:37 +0200

    Removed unreal.

--- Source/BayGanyo/BayGanyo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BayGanyo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BayGanyo, "BayGanyo" );
