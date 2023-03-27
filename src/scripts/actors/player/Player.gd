00f3a3a0491db2961fb1b50c36d75a9e46763663
Alexander Makedonski <alemak2002@gmail.com>
2023-03-27 23:55:38 +0300

    Added health to player.

--- src/scripts/actors/player/Player.gd
+++ src/scripts/actors/player/Player.gd
@@ -3,16 +3,6 @@ extends Actor
 
 class_name Player
 
-var speed: float = 1000
-var direction: Vector2 = Vector2(0,0)
+@export var health_max: int = 100
 
-func _get_keys_pressed() -> void:
-	var x_axis: = Input.get_action_strength("UI_right") - Input.get_action_strength("UI_left")
-	var y_axis: = Input.get_action_strength("UI_down") - Input.get_action_strength("UI_up")
-	direction = Vector2(x_axis, y_axis)
-
-func _physics_process(delta: float) -> void:
-	_get_keys_pressed()
-	velocity = direction.normalized()
-	velocity *= speed*delta
-	move_and_slide()
+var current_health: int = self.health_max
