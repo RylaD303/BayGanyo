4c6c189f2700342e144d132af5fcfb1d10988250
Alexander Makedonski <alemak2002@gmail.com>
2023-03-29 22:14:15 +0300

    Added hurtbox basic tests.

--- src/test/unit/test_HurtBox.gd
+++ src/test/unit/test_HurtBox.gd
@@ -1,1 +1,18 @@
 extends GutTest
+
+
+func after_all():
+	assert_no_new_orphans('There is some memory allocated still.')
+
+func test_signal_connection():
+	var hurtbox: HurtBox = autofree(HurtBox.new())
+	watch_signals(hurtbox)
+	hurtbox.emit_signal("area_entered")
+	assert_signal_emitted(hurtbox, "area_entered")
+
+func test_entering_hurtbox_with_hitbox():
+	var hurtbox: HurtBox = autofree(HurtBox.new())
+	var hitbox: HitBox = autofree(HitBox.new())
+	watch_signals(hurtbox)
+	hurtbox.emit_signal("area_entered", hitbox)
+	assert_signal_emitted(hurtbox, "area_entered", [hitbox])
