c20fc75799df2a5f2fa2166232e45c729c64a0cc
Alexander Makedonski <alemak2002@gmail.com>
2023-03-30 23:31:23 +0300

    Added evaluation of keys for player.

--- src/scripts/actors/player/Player.gd
+++ src/scripts/actors/player/Player.gd
@@ -1,3 +1,11 @@
-extends Actor
+extends CharacterBody2D
 
 class_name Player
+
+@export var speed: float = 1000
+var movement: Vector2 = Vector2(0,0)
+
+func _evaluate_keys_pressed() -> void:
+	var x_axis: = Input.get_action_strength("UI_right") - Input.get_action_strength("UI_left")
+	var y_axis: = Input.get_action_strength("UI_down") - Input.get_action_strength("UI_up")
+	self.movement = Vector2(x_axis, y_axis)
