9ecdfa7898ad656d6887ec5886dc6124efbf8308
ilian02 <ileto02@abv.bg>
2023-03-30 11:52:48 +0300

    created Bullet script

---
+++ src/scripts/actors/base_classes/Bullet.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+class_name Bullet
+
+var movement_vector := Vector2(1, 0)
+@export var speed := 300.0
+@export var delete_bullet_on_exit : bool = true
+
+
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	global_position += movement_vector.rotated(rotation) * speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if delete_bullet_on_exit:
+		queue_free()
+	
+func setup(global_position, transform_rotation):
+	self.global_position = global_position
+	self.look_at(transform_rotation)
