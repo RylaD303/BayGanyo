d1efcb590f3f34a173b9c17ab04c0ea73df3d344
Alexander Makedonski <alemak2002@gmail.com>
2023-03-30 23:58:14 +0300

    Added test for player movement.

--- src/test/unit/test_player_movement.gd
+++ src/test/unit/test_player_movement.gd
@@ -4,11 +4,18 @@ extends GutTest
 func after_all():
 	assert_no_new_orphans('There is some memory allocated still.')
 
-
 func test_player_not_moving():
 	var player: Player = autofree(Player.new())
 	player.global_position = Vector2(0, 0)
 	player.speed = 1
-	player.movement = Vector2(0,0)
 	simulate(player, 1, 1)
-	assert_eq(player.global_position, )
+	assert_eq(player.global_position, Vector2(0, 0))
+
+func test_player_moving():
+	var player: Player = autofree(Player.new())
+	player.global_position = Vector2(0, 0)
+	player.speed = 1
+	Input.action_press("UI_right")
+	simulate(player, 1, 1)
+	assert_eq(player.movement, Vector2(1, 0))
+	assert_eq(player.global_position, Vector2(1, 0))
