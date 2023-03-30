1f06ac2d7d54028f8277e95f828f12e24696974e
Alexander Makedonski <alemak2002@gmail.com>
2023-03-30 23:32:08 +0300

    Added player movement.

--- src/scripts/actors/player/Player.gd
+++ src/scripts/actors/player/Player.gd
@@ -9,3 +9,9 @@ func _evaluate_keys_pressed() -> void:
 	var x_axis: = Input.get_action_strength("UI_right") - Input.get_action_strength("UI_left")
 	var y_axis: = Input.get_action_strength("UI_down") - Input.get_action_strength("UI_up")
 	self.movement = Vector2(x_axis, y_axis)
+
+func _physics_process(delta: float) -> void:
+	_evaluate_keys_pressed()
+	velocity = self.movement.normalized()
+	velocity *= self.speed*delta
+	move_and_slide()
