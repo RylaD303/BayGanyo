4e93bbfa9ef40545e23fa72ae939ec2980a1fa63
Alexander Makedonski <alemak2002@gmail.com>
2023-04-03 23:28:18 +0300

    Added take damage for player.

--- src/scripts/actors/player/Player.gd
+++ src/scripts/actors/player/Player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 class_name Player
-
 @export var speed: int = 100
 @export var acceleration: int = 10
 @export var friction: int = 15 # how fast the player stops moving
 
 var movement: Vector2 = Vector2(0,0)
+var invincible = false
+
+func _init():
+	self.hurtbox.hitbox_entered.connect(_on_hitbox_entered)
 
 func get_input_direction() -> Vector2:
 	var x_axis: = Input.get_action_strength("UI_right") - Input.get_action_strength("UI_left")
@@ -32,3 +35,8 @@ func move() -> void:
 func _physics_process(_delta: float) -> void:
 	self.calculate_velocity()
 	self.move()
+
+func _on_hitbox_entered(hitbox: HitBox):
+	if self.invincible:
+		return
+	self.health_controller.take_damage(hitbox.get_damage())
