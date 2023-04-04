49ff51d2f7d76aa0dfabe552a4010eed0e532317
Alexander Makedonski <alemak2002@gmail.com>
2023-04-04 18:14:47 +0300

    Fixed typos in hitbox tests.

--- src/test/unit/test_hitbox.gd
+++ src/test/unit/test_hitbox.gd
@@ -3,13 +3,13 @@ extends GutTest
 func after_all():
 	assert_no_new_orphans('There is some memory allocated still.')
 
-func test_hitbox_damage_by_itself():
-	# The hitbox should't have damage by itself,
+func test_lone_hitbox_damage():
+	# The hitbox should't have damage standing by itself,
 	# only when connected to entity that has damage
 	var hitbox: Hitbox = autofree(Hitbox.new())
 	assert_eq(hitbox.get_damage(), 0)
 
-func test_hitbox_damage():
+func test_hitbox_set_damage():
 	var hitbox: Hitbox = autofree(Hitbox.new())
-	hitbox.set_damage(5)
+	hitbox.set_damage(5)\
 	assert_eq(hitbox.get_damage(), 5)
