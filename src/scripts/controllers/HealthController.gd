14fb4dd58e5027b9bc6f03ce4eebbb3287c8dcb1
Alexander Makedonski <alemak2002@gmail.com>
2023-04-04 22:05:57 +0300

    Cleaned up health controller.

--- src/scripts/controllers/HealthController.gd
+++ src/scripts/controllers/HealthController.gd
@@ -11,27 +11,23 @@ func is_health_depleted() -> bool:
 	return self.current_health <= 0
 
 func take_damage(amount: int) -> void:
-	if amount < 0:
-		return
+	amount = clamp(amount, 0, amount)
 	self.current_health -= amount
 	if self.is_health_depleted():
 		self.current_health=0
 		health_depleted.emit()
 
 func heal(amount: int) -> void:
-	if amount < 0:
-		return
+	amount = clamp(amount, 0, amount)
 	self.current_health += amount
 	if self.current_health > self.max_health:
 		self.current_health = self.max_health
 
 func set_current_health(amount: int) -> void:
-	if amount < 0:
-		return
+	amount = clamp(amount, 0, amount)
 	self.current_health = min(amount, self.max_health)
 
 func set_max_health(amount: int) -> void:
-	if amount < 0:
-		return
+	amount = clamp(amount, 0, amount)
 	self.max_health = amount
 	self.current_health = min(current_health, max_health)
