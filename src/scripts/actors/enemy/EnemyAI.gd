8af5d820318e1222563825fc29f4fa15c9a74ed0
Pamela Slavcheva <pamivladi@gmail.com>
2023-04-09 21:20:11 +0300

    Fixed bug: Timers didn't get deleted with EnemyAI.

--- src/scripts/actors/enemy/EnemyAI.gd
+++ src/scripts/actors/enemy/EnemyAI.gd
@@ -8,11 +8,12 @@ enum State{
 	ATTACKING
 }
 
-@export var lower_bound_for_idle: int = 2
-@export var higher_bound_for_idle: int = 4
-@export var lower_bound_for_wandering: int = 2
-@export var higher_bound_for_wandering: int = 4
-@export var time_in_attack_state: int = 1
+@export var speed: float = 20
+@export var lower_bound_for_idle: float = 2
+@export var higher_bound_for_idle: float = 4
+@export var lower_bound_for_wandering: float = 2
+@export var higher_bound_for_wandering: float = 4
+@export var time_in_attack_state: float = 1
 
 var gen: RandomNumberGenerator
 var state: State
@@ -34,19 +35,24 @@ func _set_state_wandering() -> void:
 	var random_x: float = gen.randf_range(0,1)
 	var random_y: float = gen.randf_range(0,1)
 	var random_velocity: Vector2 = Vector2(random_x, random_y).normalized()
-	self.velocity = random_velocity
+	self.velocity = random_velocity*self.speed
 
 func _init():
 	self.gen = RandomNumberGenerator.new()
 	self.state = State.IDLE
 	self.time_in_idle_state = gen.randf_range(lower_bound_for_idle, higher_bound_for_idle)
 	self.time_in_wandering_state = gen.randf_range(lower_bound_for_wandering, higher_bound_for_wandering)
+
+func _ready():
+	self._create_timer()
+
+func _create_timer() -> void:
 	self.timer = Timer.new()
-	add_child(self.timer)
+	self.add_child(self.timer)
 	self.timer.start(1)
+	self.timer.timeout.connect(_on_timer_timeout)
 
-
-func on_timer_timeout():
+func _on_timer_timeout() -> void:
 	if self.state == State.ATTACKING:
 		self._set_state_idle()
 		self.timer.start(self.time_in_idle_state)
@@ -57,9 +63,11 @@ func on_timer_timeout():
 		self._set_state_attacking()
 		self.timer.start(self.time_in_attack_state)
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	self.move_and_slide()
--- src/test/unit/test_enemy_AI.gd
+++ src/test/unit/test_enemy_AI.gd
@@ -9,30 +9,44 @@ func test_memory_freeing():
 	assert_no_new_orphans('There is some memory allocated still.')
 
 func test_initial_setup():
-	var enemy_AI: EnemyAI = autofree(EnemyAI.new())
+	var enemy_AI: EnemyAI = autoqfree(EnemyAI.new())
+	add_child(enemy_AI)
 	assert_eq(enemy_AI.state, EnemyAI.State.IDLE, "When first created an enemy AI is in the idle state")
 	assert_between(enemy_AI.time_in_idle_state, enemy_AI.lower_bound_for_idle, enemy_AI.higher_bound_for_idle)
 	assert_between(enemy_AI.time_in_wandering_state, enemy_AI.lower_bound_for_wandering, enemy_AI.higher_bound_for_wandering)
-	assert_eq(enemy_AI.timer.wait_time, 1)
+	assert_almost_eq(enemy_AI.timer.wait_time, 1.0, 0.01)
 
 func test_setting_state_wandering():
-	var enemy_AI: EnemyAI = autofree(EnemyAI.new())
+	var enemy_AI: EnemyAI = autoqfree(EnemyAI.new())
 	enemy_AI._set_state_wandering()
 	assert_eq(enemy_AI.state, enemy_AI.State.WANDERING)
 	assert_ne(enemy_AI.velocity, Vector2(0,0))
+	assert_almost_eq(enemy_AI.velocity.length(), enemy_AI.speed, 0.01)
 
 func test_setting_state_idle():
-	var enemy_AI: EnemyAI = autofree(EnemyAI.new())
+	var enemy_AI: EnemyAI = autoqfree(EnemyAI.new())
 	enemy_AI._set_state_idle()
 	assert_eq(enemy_AI.state, enemy_AI.State.IDLE)
 	assert_eq(enemy_AI.velocity, Vector2(0,0))
 
 func test_setting_state_attacking():
-	var enemy_AI: EnemyAI = autofree(EnemyAI.new())
+	var enemy_AI: EnemyAI = autoqfree(EnemyAI.new())
 	enemy_AI._set_state_attacking()
 	assert_eq(enemy_AI.state, enemy_AI.State.ATTACKING)
 	assert_eq(enemy_AI.velocity, Vector2(0,0))
 
 func test_changing_states_with_timer():
-	var enemy_AI: EnemyAI = autofree(EnemyAI.new())
+	var enemy_AI: EnemyAI = autoqfree(EnemyAI.new())
+	add_child(enemy_AI)
 	
+	await enemy_AI.timer.timeout
+	assert_eq(enemy_AI.state, enemy_AI.State.WANDERING)
+	assert_ne(enemy_AI.velocity, Vector2(0,0))
+	
+	await enemy_AI.timer.timeout
+	assert_eq(enemy_AI.state, enemy_AI.State.ATTACKING)
+	assert_eq(enemy_AI.velocity, Vector2(0,0))
+	
+	await enemy_AI.timer.timeout
+	assert_eq(enemy_AI.state, enemy_AI.State.IDLE)
+	assert_eq(enemy_AI.velocity, Vector2(0,0))
