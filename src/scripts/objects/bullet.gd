c6199898f79b482d131a8c2cb59e055845219f62
RylaD303 <alemak2002@gmail.com>
2023-04-15 21:50:37 +0300

    Bullet now deletes itself on enemy hit.

--- src/scripts/objects/bullet.gd
+++ src/scripts/objects/bullet.gd
@@ -5,7 +5,8 @@ class_name Bullet
 @export var hitbox: Hitbox
 var speed = 100
 func set_damage(amount: int):
-	hitbox.set_damage(amount)
+	self.hitbox.set_damage(amount)
+	self.hitbox.hit_hurtbox.connect(_hit_hurtbox)
 
 func set_starting_position(position: Vector2):
 	self.global_position = position
@@ -14,3 +15,6 @@ func set_direction_to_position(position: Vector2):
 	# setting the movement vector to be pointiong to the given position
 	self.velocity = position - self.global_position
 	self.velocity = self.velocity.normalized()*speed
+
+func _hit_hurtbox():
+	self.queue_free()
