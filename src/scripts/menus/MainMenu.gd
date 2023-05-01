fa981d021f88e2914f8b8bd7474093ce9fab9cf8
Viktor Kapra <viktorkapra@gmail.com>
2023-05-02 00:08:40 +0300

    Fixing naming and removing PlayerBullet

--- src/scripts/menus/MainMenu.gd
+++ src/scripts/menus/MainMenu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+class_name MainMenu
 
 
 func _on_button_play_pressed():
@@ -9,7 +9,7 @@ func _on_button_play_pressed():
 
 
 func _on_button_options_pressed():
-	get_tree().change_scene_to_file("res://src/objects/mainMenu/options_menu.tscn")
+	get_tree().change_scene_to_file("res://src/objects/menu/options_menu.tscn")
 
 
 func _on_button_quit_pressed():
